bb39e5c076e2902240e7e5655f74145ef88d982c
Thierri Oliveira <thierri@bitfix.com.br>
2021-03-20 19:17:52 -0300

    Methods

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,32 @@
 <template>
-  <div class="container">
-    <div class="w-1/2 mx-auto">
-      <h1 class="text-3xl">
-        nuxt-blog-2
-      </h1>
-      
+    <div class="container">
+        <div class="w-1/2 mx-auto">
+            <h1 class="text-3xl">nuxt-blog-2</h1>
+        </div>
+        <div v-for="(post, index) in posts" :key="index">
+            {{post.title}}
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 export default {
-    // created(){
-    //     console.l
-    // },
-    // methods: {
-    //     getPostData(){
-    //         return 'hello'
-    //     }
-    // }
-}
+    data() {
+        return {
+            posts: [],
+        };
+    },
+    created() {
+        this.getPostData();
+    },
+    methods: {
+        getPostData() {
+            this.$axios
+                .$get("https://api.pistonapi.com/dope-blog-2/blog-post")
+                .then((response) => {
+                    this.posts = response.data;
+                });
+        },
+    },
+};
 </script>
